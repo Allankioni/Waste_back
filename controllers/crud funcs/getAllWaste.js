38a942c01f529cf839726cd74ce677ea304d4f25
allankioni <kioniallan@gmail.com>
2024-09-30 03:19:15 +0300

    first commit

--- controllers/crud funcs/getAllWaste.js
+++
@@ -1,9 +0,0 @@
-const Waste = require("../../Schemas/Waste");
-
-const getAllWaste = async (req, res) => {
-  const wastes = await Waste.find({});
-  console.log(wastes);
-  return res.status(200).json({ wastes });
-};
-
-module.exports = getAllWaste;
